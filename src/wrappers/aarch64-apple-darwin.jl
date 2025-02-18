18b28a2da12608e586ba6aae8adba37f4bf4c7c9
jlbuild <juliabuildbot@gmail.com>
2025-02-18 19:56:33 +0000

    ZlibNG_jll build 2.2.4+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libzng,
-        "lib/libz-ng.2.2.3.dylib",
+        "lib/libz-ng.2.2.4.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
